14d75482ec097cc51ef396b97bba56e3c0baf893
Alexandr Bolotov <48355529+akademogog@users.noreply.github.com>
2022-09-13 10:45:08 +0300

    форма создания нового поста

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
-import PostList from './components/PostList';
+import React, { useState } from 'react'
+// import useRef from 'react';
+import PostList from './components/PostList'
+import MyButton from './components/UI/button/MyButton'
+import MyInput from './components/UI/input/MyInput'
 
 function App() {
   const [posts, setPosts] = useState([
@@ -8,16 +11,41 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'Javascript - язык программирования'}
   ])
 
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Python 1', body: 'Python - язык программирования'},
-    {id: 2, title: 'Python 2', body: 'Python - язык программирования'},
-    {id: 3, title: 'Python 3', body: 'Python - язык программирования'}
-  ])
+  const [post, setPost] = useState({
+    title: '',
+    body: ''
+  })
+  // const bodyInputRef = useRef() // Для неуправляемого компонента (хук useRef)
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    setPosts([...posts, {...post, id: posts.length + 1}])
+    setPost({
+      title: '',
+      body: ''
+    })
+    // console.log(bodyInputRef.current.value); // Для неуправляемого компонента (хук useRef)
+  }
 
   return (
     <div className="App">
+      <form>
+        <MyInput
+          value={post.title}
+          onInput={e => setPost({...post, title: e.target.value})}
+          type="text"
+          placeholder='Название поста'
+        />
+        <MyInput
+          // ref={bodyInputRef} // Для неуправляемого компонента (хук useRef)
+          value={post.body}
+          onInput={e => setPost({...post, body: e.target.value})}
+          type="text"
+          placeholder='Описание поста'
+        />
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title='Посты про JS'/>
-      <PostList posts={posts2} title='Посты про Python'/>
     </div>
   );
 }
