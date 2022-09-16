b9ac3d04524bb936e535763a5848a58ff886eae2
Alexandr Bolotov <48355529+akademogog@users.noreply.github.com>
2022-09-16 16:12:14 +0300

    Кастомные хуки для сортировки и поиска

--- src/App.js
+++ src/App.js
@@ -1,46 +1,16 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import PostFilter from "./components/PostFilter";
 import { PostForm } from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { usePosts } from "./hooks/usePost";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "аа",
-      body: "бб",
-    },
-    {
-      id: 2,
-      title: "гг 2",
-      body: "аа",
-    },
-    {
-      id: 3,
-      title: "вв 3",
-      body: "яя",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
-  const [visibleModal, setVisibleModal] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) =>
-        a[filter.sort].localeCompare(b[filter.sort])
-      );
-    }
-
-    return posts;
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLocaleLowerCase().includes(filter.query.toLocaleLowerCase())
-    );
-  }, [filter.query, sortedPosts]);
+  const [visibleModal, setVisibleModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -53,8 +23,13 @@ function App() {
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: '30px'}} onClick={() => setVisibleModal(true)}>Создать пост</MyButton>
-      <hr style={{margin: '10px 0px 5px'}} />
+      <MyButton
+        style={{ marginTop: "30px" }}
+        onClick={() => setVisibleModal(true)}
+      >
+        Создать пост
+      </MyButton>
+      <hr style={{ margin: "10px 0px 5px" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       <PostList
         posts={sortedAndSearchedPosts}
