629670ecae5932d3bfb9f608c65ec39841dc3bd6
Alexandr Bolotov <48355529+akademogog@users.noreply.github.com>
2022-09-16 13:32:03 +0300

    Анимация постов

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import PostItem from "../components/PostItem";
 import "../styles/App.css";
 
@@ -24,14 +25,13 @@ function PostList({ posts, title, remove }) {
       >
         {title}
       </h1>
-      {posts.map((post, index) => (
-        <PostItem
-          remove={remove}
-          number={index + 1}
-          post={post}
-          key={post.id}
-        />
-      ))}
+      <TransitionGroup>
+        {posts.map((post, index) => (
+          <CSSTransition key={post.id} timeout={500} classNames="post">
+            <PostItem remove={remove} number={index + 1} post={post} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 }
