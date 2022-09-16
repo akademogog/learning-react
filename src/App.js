7ab5bfcc0bb67199a66f3c29da46c3a099a2cdf1
Alexandr Bolotov <48355529+akademogog@users.noreply.github.com>
2022-09-16 19:31:53 +0300

    api загрузки и индикация загрузки

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
+import PostService from "./API/PostService";
 import PostFilter from "./components/PostFilter";
 import { PostForm } from "./components/PostForm";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
+import Loader from "./components/UI/loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
 import { usePosts } from "./hooks/usePost";
 
@@ -12,6 +13,7 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [visibleModal, setVisibleModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostLoading, setIsPostLoading] = useState(false);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -19,14 +21,16 @@ function App() {
   };
 
   async function fetchPosts() {
-    const respons = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    setPosts(respons.data);
+    setIsPostLoading(true);
+    const posts = await PostService.getAll();
+    setPosts(posts);
+    setIsPostLoading(false);
   }
 
   useEffect(() => {
     fetchPosts();
-  }, [])
-  
+  }, []);
+
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
@@ -39,16 +43,20 @@ function App() {
       >
         Создать пост
       </MyButton>
-      <hr style={{ margin: "10px 0px 5px" }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        posts={sortedAndSearchedPosts}
-        remove={removePost}
-        title="Посты про JS"
-      />
       <MyModal visible={visibleModal} setVisible={setVisibleModal}>
         <PostForm posts={posts} create={createPost} />
       </MyModal>
+      <hr style={{ margin: "10px 0px 5px" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      {isPostLoading ? (
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}><Loader/></div>
+      ) : (
+        <PostList
+          posts={sortedAndSearchedPosts}
+          remove={removePost}
+          title="Посты про JS"
+        />
+      )}
     </div>
   );
 }
