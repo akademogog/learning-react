0ecaff9fab9299dde649db24b7f497c3e816fb4b
Alexandr Bolotov <48355529+akademogog@users.noreply.github.com>
2022-09-17 13:16:17 +0300

    useFetching кастомный хук для обработки загрузки

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -2,13 +2,9 @@ import axios from "axios";
 
 export default class PostService {
   static async getAll() {
-    try {
-      const respons = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      return respons.data;
-    } catch (error) {
-      console.log(error);
-    }
+    const respons = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    return respons.data;
   }
 }
--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import Loader from "./components/UI/loader/Loader";
 import MyModal from "./components/UI/MyModal/MyModal";
+import { useFetching } from "./hooks/useFetching";
 import { usePosts } from "./hooks/usePost";
 
 function App() {
@@ -13,23 +14,19 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [visibleModal, setVisibleModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostLoading, setIsPostLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setVisibleModal(false);
   };
-
-  async function fetchPosts() {
-    setIsPostLoading(true);
-    const posts = await PostService.getAll();
-    setPosts(posts);
-    setIsPostLoading(false);
-  }
-
-  useEffect(() => {
-    fetchPosts();
-  }, []);
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -48,8 +45,17 @@ function App() {
       </MyModal>
       <hr style={{ margin: "10px 0px 5px" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      {isPostLoading ? (
-        <div style={{display: 'flex', justifyContent: 'center', marginTop: '20px'}}><Loader/></div>
+      {postError && <h1>Ошибка загрузки ${postError}</h1>}
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          <Loader />
+        </div>
       ) : (
         <PostList
           posts={sortedAndSearchedPosts}
